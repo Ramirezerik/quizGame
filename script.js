6ed452494f0a166e96459e8635792b348c6576dd
Erik Ramirez <84946446+Ramirezerik@users.noreply.github.com>
2023-01-03 19:59:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+//when startbutton is clicked, this will cue code for startGame function
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'Which program langauge is the most populat?', 
+        answers: [
+            {text: 'Python', correct: true },
+            {text: 'Javascript', correct: false },
+            {text: 'C++', correct: false},
+            {txt: 'Java', correct: false}
+        ]
+    },
+
+    {
+        question: 'What is the most popular sport in the world?',
+        answers: [
+            { text: 'Baseball', correct: false },
+            { text: 'Tennis', correct: false },
+            { text: 'Football', correct: false },
+            { text: 'Soccer', correct: true }
+        ]
+    },
+    {
+        question: 'Is web development fun?',
+        answers: [
+            { text: 'Kinda', correct: false },
+            { text: 'YES!!!', correct: true },
+            { text: 'Um no', correct: false },
+            { text: 'IDK', correct: false }
+        ]
+    },
+    {
+        question: 'What is 4 * 2?',
+        answers: [
+            { text: '6', correct: false },
+            { text: '8', correct: true }
+        ]
+    }
+]
